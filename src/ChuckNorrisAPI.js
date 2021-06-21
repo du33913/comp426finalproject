340d53b1ca2a19c852d8cd8b205b9f9ba342aa99
Kyle <du33913@live.unc.edu>
2021-06-21 03:16:23 -0700

    yikes

---
+++ src/ChuckNorrisAPI.js
@@ -0,0 +1,51 @@
+import React, {useState} from "react";
+import "./ChuckNorrisAPI.css"
+
+function ChuckNorrisAPI(props) {
+
+    let [responseObj, setResponseObj] = useState({});
+
+    function getJoke() {
+        fetch("http://api.icndb.com/jokes/random?escape=javascript", {
+            "method": "GET",
+        })
+            .then(response => response.json())
+            .then(response => {
+                setResponseObj(response);
+            })
+            .catch(err => {console.log(err);
+            })
+    }
+
+
+
+    return (
+        <div className="App">
+            <main>
+                <div>
+                    {"Click on the button to get a Chuck Norris joke that's guaranteed to make you smirk. You'll get a new one each time :)"}
+                    <br/>
+                    <button className={"button"} onClick={getJoke}>Get Joke</button>
+                    <br/>
+                    <Conditions responseObj={responseObj}/>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+function Conditions(props) {
+    let checker = JSON.stringify(props.responseObj)
+    if (checker === '{}') {
+        return "";
+    } else {
+        let obj = JSON.parse(checker);
+        return (
+            <div>
+                {obj.value.joke}
+            </div>
+        )
+    }
+}
+
+export default ChuckNorrisAPI;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
 import Game from './Othello'
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
